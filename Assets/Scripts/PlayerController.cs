5d49cd73463546458ecc43a831e4e85dcc5312ba
LonelyThic <181782450+LonelyThic@users.noreply.github.com>
2026-09-23 20:27:42 -0700

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,29 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent <Rigidbody>();
     }
 
-    // Update is called once per frameC:\Users\burn2\Downloads\unity\unity 6\Roll-A-Ball\Assets\Scripts\PlayerController.cs
-    void Update()
+    void FixedUpdate()
     {
-        
+        rb.AddForce(movement);
+        Vector3 movement = new Vector3 ( movementX, 2.0f, movementY);
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
     }
 }
