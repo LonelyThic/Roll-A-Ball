283eb8bd9d22444ad56b9eadba01fdee72fe9e40
LonelyThic <181782450+LonelyThic@users.noreply.github.com>
2026-09-23 20:18:52 -0700

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frameC:\Users\burn2\Downloads\unity\unity 6\Roll-A-Ball\Assets\Scripts\PlayerController.cs
+    void Update()
+    {
+        
+    }
+}
